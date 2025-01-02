d3724b952d266bfad8680dd0eb31a1922d9b70b4
iamabdurrasheed <22bd1a0538@gmail.com>
2025-01-02 19:37:37 +0530

    committed maven project

---
+++ src/main/java/com/org/hello/mavenjavapractice/App.java
@@ -0,0 +1,10 @@
+package com.org.hello.mavenjavapractice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
